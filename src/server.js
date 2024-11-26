5b834e89a0a05e72467983b767f298f189b5c85e
Petar Markota <pmarko1@racunarstvo.hr>
2024-11-26 13:06:11 +0100

    v

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-const http = require("http");
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-
-// Create and start the server
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://mini.local:${PORT}`);
-});
+const http = require("http");
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+// Create and start the server
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://mini.local:${PORT}`);
+});
+

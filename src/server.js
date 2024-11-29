a16a68d33bb11ffd69d856874021e07c76be46ff
Petar Markota <pmarko1@racunarstvo.hr>
2024-11-29 15:02:05 +0100

    .

--- src/server.js
+++ src/server.js
@@ -2,30 +2,20 @@ const http = require("http");
 
 const app = require("./app");
 
-
-
 const PORT = process.env.PORT || 3000;
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
-const domain = isProduction ? 'tamilfreelancer.rest' : 'mini.local';
-
-
+const domain = isProduction ? "tamilfreelancer.rest" : "mini.local";
 
 // Create and start the server
 
 const server = http.createServer(app);
 
-
-
 server.listen(PORT, () => {
-
-  console.log(`Server running on ${isProduction ? 'https' : 'http'}://${domain}:${PORT}`);
-
+  console.log(
+    `Server running on ${isProduction ? "https" : "http"}://${domain}:${PORT}`
+  );
 });
 
-
-
 module.exports = app;
-
-
